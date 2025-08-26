9d79c21cd6a8b5f148882ff7bfb6dac9d29b89bc
Sang.Mac <sangmin@sangmacui-noteubug.local>
2025-08-26 17:13:56 +0900

    Fix export names in index.ts for build compatibility

--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,9 @@
 export { UpstageApi } from './credentials/UpstageApi.credentials';
 
 // Export all nodes
-export { LmChatUpstage } from './nodes/LmChatUpstage/LmChatUpstage.node';
+export { LmRequestUpstage } from './nodes/LmChatUpstage/LmChatUpstage.node';
 export { EmbeddingsUpstage } from './nodes/EmbeddingsUpstage/EmbeddingsUpstage.node';
 
 // Export LangChain compatible nodes - using alias to avoid duplicate
-export { LmChatUpstage as LmChatModelUpstage } from './nodes/LmChatModelUpstage/LmChatModelUpstage.node';
+export { LmChatUpstage } from './nodes/LmChatModelUpstage/LmChatModelUpstage.node';
 export { EmbeddingsUpstageModel } from './nodes/EmbeddingsUpstageModel/EmbeddingsUpstageModel.node';
